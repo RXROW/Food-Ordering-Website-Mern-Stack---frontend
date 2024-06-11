ac0ea0607ac53bcb712874d103543eb6e269173e
“Eslam“ <aslamalmswdya@gmail.com>
2024-06-11 16:43:53 +0300

    Next & Prev Btns In Slider

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -26,7 +26,9 @@ return (
     {/* </Link> */}
 
   <div className="card-body">
+    {/* <Link to={`/menu/${item._id}`}> */}
     <h2 className="card-title">{item.name}</h2>
+      {/* </Link> */}
     <p>{item.recipe}</p>
     <div className="card-actions justify-between items-center mt-5 ">
       <h5 className=" font-semibold"><span className="p-0 text-sm text-rose-600 ">$</span>{item.price}</h5>
--- src/pages/home/SpacialDishes.jsx
+++ src/pages/home/SpacialDishes.jsx
@@ -1,12 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
- 
+import "slick-carousel/slick/slick-theme.css"; 
 import Slider from "react-slick";
 import Cards from '../../components/Cards';
+import { FaA } from 'react-icons/fa6';
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+
+const simpleNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    >
+      Next
+    </div>
+  );
+}
+
+const simplePrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    >
+      Back
+    </div>
+  );
+}
 
 const SpacialDishes = () => {
   const [recipes, setRecipes] = useState([]);
+  const slider = useRef(null);
 
   useEffect(() => {
     fetch("/menu.json")
@@ -17,13 +45,15 @@ const SpacialDishes = () => {
       });
   }, []);
 
-  var settings = {
+  const settings = {
     dots: true,
     infinite: false,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
     initialSlide: 0,
+    nextArrow: <simpleNextArrow />,
+    prevArrow: <simplePrevArrow />,
     responsive: [
       {
         breakpoint: 1024,
@@ -53,11 +83,17 @@ const SpacialDishes = () => {
   };
 
   return (
-    <div className='sectionContainer my-30 '>
+    <div className='sectionContainer my-30 relative'>
       <div className='text-left'>
         <p className='subTitle mt-2'>Special Dishes</p>
         <h2 className='title md:w-[520px]'>Standout Dishes From Our Menu </h2>
-        <Slider {...settings} className='py-10'>
+        <div className='md:absolute right-3 top-8'>
+          <button onClick={() => slider.current.slickPrev()} className='btn p-2 rounded-sm bg-rose-500 text-white hover:bg-rose-800'> 
+          <FaAngleLeft className='w-8 h-8 p-1'/> </button>
+          <button onClick={() => slider.current.slickNext()} className='btn p-2 rounded-sm  bg-rose-500 text-white hover:bg-rose-800'>
+          <FaAngleRight className='w-8 h-8 p-1'/> </button>
+        </div>
+        <Slider ref={slider} {...settings} className='py-10'>
           {recipes.map((item, i) => (
             <div key={i}>
               <Cards item={item} />
